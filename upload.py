5c06c1cf33d8f21983a2b7e2761132a33d5fa6df
RoseOu <598959770@qq.com>
2017-03-05 13:24:02 +0800

    fix

--- upload.py
+++ upload.py
@@ -3,16 +3,15 @@ import os
 from flask import Flask, request, send_from_directory,jsonify
 import time
 
-
 UPLOAD_FOLDER='guisheng_pics'
-ALLOWED_EXTENSIONS=set(['png','jpg','jpeg'])
+ALLOWED_EXTENSIONS=set(['png','jpg','jpeg','svg'])
 
 app = Flask(__name__)
 @app.route('/guisheng/upload_pics/',methods = ['GET','POST'])
 def upload_pic():
     if request.method == 'POST':
         file = request.files['file']
-        if file:#and ('.' in filename and filename.split('.',1)[1] in ALLOWED_EXTENSIONS):
+        if file and ('.' in file.filename and file.filename.split('.',1)[1] in ALLOWED_EXTENSIONS):
             fname = '.'.join([str(int(time.time())),file.filename.split('.',1)[1]])
             file.save(os.path.join(UPLOAD_FOLDER,fname))
             pic_url = os.path.join('http://120.24.4.254:7777/',UPLOAD_FOLDER,fname)
@@ -23,10 +22,6 @@ def upload_pic():
 @app.route('/guisheng_pics/<filename>/',methods = ['GET','POST'])
 def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER,filename)
-
-@app.route('/')
-def index():
-    return "hello"
 
 if __name__ == '__main__':
     app.debug = True
