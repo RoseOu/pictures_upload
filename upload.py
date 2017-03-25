5171e4c452ba2b755856f16534579f9b0189baab
roseou <598959770@qq.com>
2017-03-25 13:42:29 +0800

    add delete_pics

--- upload.py
+++ upload.py
@@ -3,13 +3,12 @@ import os
 from flask import Flask, request, send_from_directory,jsonify
 import time
 
-
 UPLOAD_FOLDER='guisheng_pics'
-ALLOWED_EXTENSIONS=set(['png','jpg','jpeg'])
+ALLOWED_EXTENSIONS=set(['png','jpg','jpeg','svg'])
 
 app = Flask(__name__)
-@app.route('/guisheng/upload_pics/',methods = ['GET','POST'])
-def upload_pic():
+@app.route('/guisheng/upload_pics/',methods = ['POST'])
+def upload_pics():
     if request.method == 'POST':
         file = request.files['file']
         if file:#and ('.' in filename and filename.split('.',1)[1] in ALLOWED_EXTENSIONS):
@@ -20,13 +19,17 @@ def upload_pic():
             'pic_url':pic_url
         })
 
+@app.route('/guisheng/delete_pics/',methods = ['POST'])
+def delete_pics():
+    if request.method == 'POST':
+        img_url = request.get_json().get('img_url')
+        filename = img_url.split('/')[-1]
+        os.remove(os.path.join(UPLOAD_FOLDER,filename))
+        return jsonify({'filename':filename}),200
+
 @app.route('/guisheng_pics/<filename>/',methods = ['GET','POST'])
 def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER,filename)
-
-@app.route('/')
-def index():
-    return "hello"
 
 if __name__ == '__main__':
     app.debug = True
